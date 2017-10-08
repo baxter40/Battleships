ff015c6faaebccec84f66870ac073461b4f90429
Hamish Baxter <hamish_baxter@hotmail.co.uk>
2017-10-08 17:26:37 +0100

    Added numbers printed around the edge of the game board

--- src/com/hamish/battleships/GameBoard.java
+++ src/com/hamish/battleships/GameBoard.java
@@ -113,9 +113,11 @@ public class GameBoard {
      * Prints out the game board
      */
     public void printBoard() {
+        System.out.println(" \t1 2 3 4 5 6 7 8 9 10");
         for (int i = 0; i < 10; i++) {
+            System.out.print(i + 1 + "\t");
             for (int j = 0; j < 10; j++) {
-                System.out.print(board[i][j]);
+                System.out.print(board[i][j] + " ");
             }
             System.out.print("\n");
         }
